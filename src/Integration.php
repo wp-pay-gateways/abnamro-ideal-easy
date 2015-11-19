90157486358eae133dd9e4e7bb1395b84441b65d
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 08:17:43 +0100

    Extend from Ogone integration class.

--- src/Integration.php
+++ src/Integration.php
@@ -1,22 +1,10 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_AbnAmro_IDealEasy_Integration {
+class Pronamic_WP_Pay_Gateways_AbnAmro_IDealEasy_Integration extends Pronamic_WP_Pay_Gateways_Ogone_OrderStandardEasy_Integration {
 	public function __construct() {
 		$this->id       = 'abnamro-ideal-easy';
 		$this->name     = 'ABN AMRO - iDEAL Easy';
 		$this->url      = 'https://internetkassa.abnamro.nl/';
 		$this->provider = 'abnamro';
-	}
-
-	public function get_config_factory_class() {
-		return 'Pronamic_WP_Pay_Gateways_Ogone_OrderStandardEasy_ConfigFactory';
-	}
-
-	public function get_config_class() {
-		return 'Pronamic_WP_Pay_Gateways_Ogone_OrderStandardEasy_Config';
-	}
-
-	public function get_gateway_class() {
-		return 'Pronamic_WP_Pay_Gateways_Ogone_OrderStandardEasy_Gateway';
 	}
 }
