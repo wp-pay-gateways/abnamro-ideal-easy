d45736158d99ab5ad5c3cce408fd2f76c33189f0
Remco Tolsma <info@remcotolsma.nl>
2016-01-29 15:46:26 +0100

    Getting ready for version 1.0.0.

--- src/Integration.php
+++ src/Integration.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * Title: ABN AMRO - iDEAL Easy integration
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2016
+ * Company: Pronamic
+ *
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
 class Pronamic_WP_Pay_Gateways_AbnAmro_IDealEasy_Integration extends Pronamic_WP_Pay_Gateways_Ogone_OrderStandardEasy_Integration {
 	public function __construct() {
 		$this->id       = 'abnamro-ideal-easy';
