aa79c5900c45ae0b92fdb0778d9fb03a20efcc16
Remco Tolsma <info@remcotolsma.nl>
2016-02-12 09:11:33 +0100

    Extend from the Ingenico/Ogone Easy config class.

--- src/Config.php
+++ src/Config.php
@@ -7,9 +7,10 @@
  * Company: Pronamic
  *
  * @author Remco Tolsma
- * @version 1.0.0
+ * @version 1.0.1
+ * @since 1.0.0
  */
-class Pronamic_WP_Pay_Gateways_AbnAmro_IDealEasy_Config extends Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_Config {
+class Pronamic_WP_Pay_Gateways_AbnAmro_IDealEasy_Config extends Pronamic_WP_Pay_Gateways_Ogone_OrderStandardEasy_Config {
 	public function get_payment_server_url() {
 		return 'https://internetkassa.abnamro.nl/ncol/prod/orderstandard.asp';
 	}
