411db7f6626de9f4cd0bc17bfa0ddd766ecccd86
Remco Tolsma <info@remcotolsma.nl>
2016-03-31 14:40:39 +0200

    Make sure to extend from the easy config factory class.

--- src/ConfigFactory.php
+++ src/ConfigFactory.php
@@ -9,7 +9,7 @@
  * @author Remco Tolsma
  * @version 1.0.0
  */
-class Pronamic_WP_Pay_Gateways_AbnAmro_IDealEasy_ConfigFactory extends Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_ConfigFactory {
+class Pronamic_WP_Pay_Gateways_AbnAmro_IDealEasy_ConfigFactory extends Pronamic_WP_Pay_Gateways_Ogone_OrderStandardEasy_ConfigFactory {
 	public function __construct() {
 		parent::__construct( 'Pronamic_WP_Pay_Gateways_AbnAmro_IDealEasy_Config', 'Pronamic_WP_Pay_Gateways_AbnAmro_IDealEasy_TestConfig' );
 	}
