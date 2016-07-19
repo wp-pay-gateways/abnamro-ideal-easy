6d0a6db175a241a6d23ea299957abd79a3d7b485
Reüel van der Steege <reuel@pronamic.nl>
2016-07-19 12:29:56 +0200

    Set default form action URL

--- src/Config.php
+++ src/Config.php
@@ -11,6 +11,22 @@
  * @since 1.0.0
  */
 class Pronamic_WP_Pay_Gateways_AbnAmro_IDealEasy_Config extends Pronamic_WP_Pay_Gateways_Ogone_OrderStandardEasy_Config {
+	/**
+	 * Get the default Ogone e-Commerce form action URL.
+	 *
+	 * @since unreleased
+	 * @return string
+	 */
+	protected function get_default_form_action_url() {
+		$is_utf8 = strcasecmp( get_bloginfo( 'charset' ), 'UTF-8' ) === 0;
+
+		if ( $is_utf8 ) {
+			return 'https://internetkassa.abnamro.nl/ncol/prod/orderstandard_utf8.asp';
+		}
+
+		return 'https://internetkassa.abnamro.nl/ncol/prod/orderstandard.asp';
+	}
+
 	public function get_payment_server_url() {
 		return 'https://internetkassa.abnamro.nl/ncol/prod/orderstandard.asp';
 	}
